fcf8e75d77c6eb5beb4ce5735990a3002a1b766f
ytorium <ytorium@gmail.com>
2023-04-14 19:25:41 +0300

    Commit 8

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,7 +7,7 @@ export default NextAuth({
   providers: [
     TwitterProvider({
       clientId: process.env.TWITTER_CONSUMER_KEY,
-      clientSecret: process.env.TWITTER_CONSUMER_SECRET.
+      clientSecret: process.env.TWITTER_CONSUMER_SECRET,
       version: "2.0"
     }),
     GithubProvider({
